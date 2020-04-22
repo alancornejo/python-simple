094f872564aff00485e9a2785b94656285846b71
Alan Cornejo <alancornejo@icloud.com>
2020-04-22 16:33:46 -0500

    Aplicando variables de entorno

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
+from dotenv import load_dotenv
+load_dotenv()
+name = os.getenv("nombre", 8081)
+
 @app.route("/")
 def hello():
-    return "Hello World Alan Cornejo!"
+    return f"Hello World mi nombre es : {name}!"
