98d890a86322ef3e5d686012f76b95fe76e59a4d
Alan Cornejo <alancornejo@icloud.com>
2020-04-22 16:39:31 -0500

    Correción

--- application.py
+++ application.py
@@ -2,9 +2,7 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-from dotenv import load_dotenv
-load_dotenv()
-name = os.getenv("nombre", 8081)
+name = os.environ['nombre']
 
 @app.route("/")
 def hello():
